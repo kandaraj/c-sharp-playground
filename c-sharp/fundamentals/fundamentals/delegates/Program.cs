f880d0599d8940d66233c247e11294179ce252be
Siva Kandaraj <siva.kandaraj@gmail.com>
2021-07-16 19:29:58 +1000

    simplified lambda

--- c-sharp/fundamentals/fundamentals/delegates/Program.cs
+++ c-sharp/fundamentals/fundamentals/delegates/Program.cs
@@ -5,8 +5,8 @@ Console.WriteLine("Hello World!");
 
 var mathServ = new MathService();
 
-mathServ.MathPerformed += (object? sender, MathPerformedEventArgs e) 
-    => Console.WriteLine("Result from lambda " + e.Result);
+mathServ.MathPerformed += (_, e) 
+    => Console.WriteLine("Result from simplified lambda " + e.Result);
  
 
 mathServ.MultiplyNumbers(32.323, 3423.23);
