786e55f5628bf45a998899342782809827f2e05a
Siva Kandaraj <siva.kandaraj@gmail.com>
2021-07-16 19:28:37 +1000

    to lambda

--- c-sharp/fundamentals/fundamentals/delegates/Program.cs
+++ c-sharp/fundamentals/fundamentals/delegates/Program.cs
@@ -5,9 +5,8 @@ Console.WriteLine("Hello World!");
 
 var mathServ = new MathService();
 
-mathServ.MathPerformed += delegate(object? sender, MathPerformedEventArgs e)
-{
-    Console.WriteLine("Result from delegate " + e.Result);
-};
+mathServ.MathPerformed += (object? sender, MathPerformedEventArgs e) 
+    => Console.WriteLine("Result from lambda " + e.Result);
+ 
 
 mathServ.MultiplyNumbers(32.323, 3423.23);
