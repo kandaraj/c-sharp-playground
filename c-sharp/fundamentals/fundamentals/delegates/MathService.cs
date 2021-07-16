c65fbefad30ea8ba2a2a2e54781fbf0d924570bb
Siva Kandaraj <siva.kandaraj@gmail.com>
2021-07-16 19:48:28 +1000

    Action in place

--- c-sharp/fundamentals/fundamentals/delegates/MathService.cs
+++ c-sharp/fundamentals/fundamentals/delegates/MathService.cs
@@ -4,16 +4,14 @@ namespace delegates
 {
     public class MathService
     {
-        public delegate void MathFormedHandler(double result);
+        public Action<double> MathPerFormed;
 
         public delegate double CalculationHandler(double x, double y);
-
-        public event MathFormedHandler MathPerformed;
         
         public void CalculateNumbers(double x, double y, CalculationHandler calculation)
         {
             var result = calculation(x, y);
-            MathPerformed(result);
+            MathPerFormed(result);
         }
     }
 }
--- c-sharp/fundamentals/fundamentals/delegates/Program.cs
+++ c-sharp/fundamentals/fundamentals/delegates/Program.cs
@@ -5,6 +5,6 @@ Console.WriteLine("Hello World!");
 
 var mathServ = new MathService();
 
-mathServ.MathPerformed += (result) => Console.WriteLine("Result from execution " + result);
+mathServ.MathPerFormed = (result) => Console.WriteLine("Result from Action " + result);
 
 mathServ.CalculateNumbers(32.323, 3423.23, (x, y) => x * y);
