3193dcebc45bae9dbcddcc7ca275f7ba352ef63a
Siva Kandaraj <siva.kandaraj@gmail.com>
2021-07-17 10:42:14 +1000

    Tuples

---
+++ c-sharp/fundamentals/fundamentals/tuples/Point.cs
@@ -0,0 +1,12 @@
+namespace tuples
+{
+    public readonly struct Point
+    {
+        public Point(double d)
+        {
+            X = d;
+        }
+
+        public double X { get;  }
+    }
+}
---
+++ c-sharp/fundamentals/fundamentals/tuples/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using tuples;
+
+Console.WriteLine("Tuples");
+
+// null check 
+string s = "null";
+_ = s ?? throw new Exception("It is null");
+
+// tuples switch
+
+var bool1 = true;
+var bool2 = "Siva";
+
+var value = (bool1, bool2) switch {
+    (true, "siva") =>  1,
+    (false, "siva") => 0,
+    (_, _) => 12
+};
+
+Console.WriteLine(value);
+
+// using struct
+var point = new Point(1021.12);
+
+Console.WriteLine($"Point X {point.X}");
